21b803dd0077e3b9860fdf5b4d2d4a36ceec6bc7
Akram Ammis <122386669+AkramAmmis@users.noreply.github.com>
2025-11-03 12:57:22 +0100

    Add @ResponseStatus to createProject, add test for 201 status.

--- src/main/java/de/szut/lf8_starter/project/ProjectController.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectController.java
@@ -3,15 +3,9 @@ package de.szut.lf8_starter.project;
 import de.szut.lf8_starter.project.DTO.ProjectCreateDto;
 import de.szut.lf8_starter.project.DTO.ProjectResponseDto;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/projects")
@@ -24,15 +18,13 @@ public class ProjectController {
     }
 
     @PostMapping
-    public ProjectResponseDto createProject(
-            @Valid @RequestBody ProjectCreateDto dto) {
+    @ResponseStatus(HttpStatus.CREATED)  // 👈 Das ist der Trick!
+    public ProjectResponseDto createProject(@Valid @RequestBody ProjectCreateDto dto) {
         return service.create(dto);
-      // 201 + Location + Body
     }
 
     @GetMapping("/{id}")
     public ProjectResponseDto getProjectById(@PathVariable long id){
         return service.getById(id);
     }
-
 }
