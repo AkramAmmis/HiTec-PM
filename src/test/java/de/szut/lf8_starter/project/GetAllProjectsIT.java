3c5f03c7904b12fbfd9d9ca5c0253772107f9e6e
taha-yasin.ipek <taha-yasin.ipek@hmmh.de>
2025-11-04 22:54:08 +0100

    test(projekt): Sicherstellung, dass der Testfall die Rückgabe von HTTP 200 und die korrekte Struktur und Vollständigkeit der Projektdaten überprüft + Anpassungen & Fixes.

--- src/test/java/de/szut/lf8_starter/project/GetAllProjectsIT.java
+++ src/test/java/de/szut/lf8_starter/project/GetAllProjectsIT.java
@@ -2,25 +2,54 @@ package de.szut.lf8_starter.project;
 
 import de.szut.lf8_starter.testcontainers.AbstractIntegrationTest;
 
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
 
 public class GetAllProjectsIT extends AbstractIntegrationTest {
-    ProjectEntity entity = new ProjectEntity();
+    ProjectEntity project1 = new ProjectEntity();
+    ProjectEntity project2 = new ProjectEntity();
+    ProjectEntity project3 = new ProjectEntity();
 
-    public void insertTestProject() {
-        entity.setId(1L);
-        entity.setBezeichnung("Nebula Nexus");
-        entity.setKundenId(1L);
-        entity.setVerantwortlicherMitarbeiterId(1L);
-        entity.setStartdatum(LocalDate.of(2025, 4, 18));
-        entity.setGeplantesEnddatum(LocalDate.of(2025, 10, 18));
-        entity.setBeschreibung("Entwicklung einer cloudbasierten Plattform zur zentralen Verwaltung und Analyse interstellarer Datenströme.");
+    public void insertTestProjects() {
+        project1.setId(1L);
+        project1.setBezeichnung("Nebula Nexus");
+        project1.setKundenId(1L);
+        project1.setVerantwortlicherMitarbeiterId(1L);
+        project1.setStartdatum(LocalDate.of(2025, 4, 18));
+        project1.setGeplantesEnddatum(LocalDate.of(2025, 10, 18));
+        project1.setBeschreibung("Entwicklung einer cloudbasierten Plattform zur zentralen Verwaltung und Analyse interstellarer Datenströme.");
+
+        project2.setId(2L);
+        project2.setBezeichnung("Orion Ops");
+        project2.setKundenId(2L);
+        project2.setVerantwortlicherMitarbeiterId(2L);
+        project2.setStartdatum(LocalDate.of(2025, 5, 10));
+        project2.setGeplantesEnddatum(LocalDate.of(2025, 11, 20));
+        project2.setBeschreibung("Entwicklung eines automatisierten Deployment systems für interplanetare Serverflotten.");
+
+        project3.setId(3L);
+        project3.setBezeichnung("Lunar Logic");
+        project3.setKundenId(3L);
+        project3.setVerantwortlicherMitarbeiterId(3L);
+        project3.setStartdatum(LocalDate.of(2025, 2, 5));
+        project3.setGeplantesEnddatum(LocalDate.of(2025, 8, 15));
+        project3.setBeschreibung("KI-gestützte Optimierung von Kommunikationsrouten zwischen Mondbasen und Erdstationen.");
     }
 
-    public void getAllProjects_returnAllProjects() throws Exception {
-        mockMvc.perform(get("/api/v1/projects")).andExpect(status().isOk());
+    public void getAllProjects_returnsAllProjects() throws Exception {
+        mockMvc.perform(get("/api/v1/projects"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[*].bezeichnung", hasItem("Nebula Nexus")))
+                .andExpect(jsonPath("$[*].id").exists())
+                .andExpect(jsonPath("$[*].bezeichnung").exists())
+                .andExpect(jsonPath("$[*].kundenId").exists());
+
+        ;
     }
 }
